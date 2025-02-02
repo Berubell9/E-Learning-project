4150637c813d51762ba899c67d9c55157215ea0d
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2025-02-02 18:58:33 +0700

    version 1

--- src/pages/Test/After.jsx
+++ src/pages/Test/After.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Content from "../../components/Test/After/Content";
+import Footer from '../../components/Footer';
 
 function After() {
   return (
     <div className="bg-gray-100 min-h-screen w-full">
       <Navbar />
       <Content/>
+      <Footer/>
     </div>
   );
 }
--- src/pages/Test/Before.jsx
+++ src/pages/Test/Before.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Content from "../../components/Test/Before/Content";
+import Footer from '../../components/Footer';
 
 function Before() {
   return (
     <div className="bg-gray-100 min-h-screen w-full">
       <Navbar />
       <Content/>
+      <Footer/>
     </div>
   );
 }
